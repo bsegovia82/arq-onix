eb5865b28d6147c71c1ed3fd1050c3a8ab4909e9
Byron Santiago Segovia Quintero <Byron Santiago Segovia Quintero>
2017-02-14 14:52:24 -0500

    Final

--- onix-librerias-producto/onix-modulo-librerias-servicio/src/main/java/com/onix/modulo/librerias/eao/GenericEAO.java
+++ onix-librerias-producto/onix-modulo-librerias-servicio/src/main/java/com/onix/modulo/librerias/eao/GenericEAO.java
@@ -126,7 +126,7 @@ public abstract class GenericEAO<ENTIDAD extends IEntidadPersistible<Id>, Id ext
 		}
 
 		if (pidReferencia != null) {
-			query += " and " + "modelo.idReferencia = (select t.id_referencia from oms_usuarios t "
+			query += " and " + "modelo.idReferencia = (select t.idReferencia from OmsUsuario t "
 					+ " where t.usuario = '" + pidReferencia + "'" + " and t.estado = 'A') ";
 		}
 		if (orderFecha) {
